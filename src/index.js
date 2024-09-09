3e74afa3d00f89a0c74c30e267f7554797173af6
anuxr4g <anun20003@gmail.com>
2024-09-09 14:19:46 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import "slick-carousel/slick/slick.css";
+import React from "react"
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import "./Theme/theme.css"
+import reportWebVitals from './reportWebVitals';
+import { configureStore } from "@reduxjs/toolkit";
+import allReducers from "./Redux/reducers";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import "./index.css";
-import App from "./App";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from "./Routes/ScrollToTop";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+
+export const store = configureStore(
+  {
+    reducer: allReducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      serializableCheck: false,
+    })
+  },
+  applyMiddleware(thunk),
+  // chrome extension for redux
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
