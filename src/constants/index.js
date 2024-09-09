a97cec69e5b0aaf2aacd2e9ca4b11264256ccdb8
anuxr4g <anun20003@gmail.com>
2024-09-09 14:11:55 +0530

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+export const navBarList = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "/",
+  },
+];
+
+export const SplOfferData = [
+  {
+    _id: "65d78dcf8d90eb34605cd027",
+    image: "https://res.cloudinary.com/dqbol0tys/image/upload/v1708638355/1-1_xov6ea.png",
+    productName: "Dolce & Gabanna",
+    price: "35.00",
+    color: "Blank and White",
+    badge: true,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+  },
+  {
+    _id: "65d78dcf8d90eb34605cd028",
+    image: "https://res.cloudinary.com/dqbol0tys/image/upload/v1708638355/2-2_bztasa.png",
+    productName: "Michael Kors",
+    price: "180.00",
+    color: "Gray",
+    badge: true,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+  },
+  {
+    _id: "65d89bd38d90eb34605cd047",
+    image: "https://res.cloudinary.com/dqbol0tys/image/upload/v1708694353/N4-1_rgrc3q.png",
+    productName: "Armo Feragamo",
+    price: "35.00",
+    color: "Purple",
+    badge: true,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+  },
+  {
+    _id: "65d89c088d90eb34605cd048",
+    image: "https://res.cloudinary.com/dqbol0tys/image/upload/v1708694356/N5-1_inlnib.png",
+    productName: "Sigorina",
+    price: "80.00",
+    color: "Black",
+    badge: true,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+  }
+  
+];
